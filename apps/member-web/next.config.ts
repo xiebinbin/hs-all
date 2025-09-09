b91cfe52eea1037064f9243738bec4a27c71fbec
Your Name <you@example.com>
2025-09-09 16:29:16 +0800

    fix(member-web): 允许所有开发源访问以简化开发环境配置

--- apps/member-web/next.config.ts
+++ apps/member-web/next.config.ts
@@ -1,8 +1,7 @@
 import type { NextConfig } from "next";
-
 const nextConfig: NextConfig = {
   /* config options here */
-  allowedDevOrigins: ['m.haishangjiayi.cn','localhost'],
+  
+  allowedDevOrigins: ['*'],
 };
-
 export default nextConfig;
